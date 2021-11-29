6492ec412a6914ed7546af475d806b28f2609d26
Sam Butler <sam.butler@softwire.com>
2021-11-29 15:33:56 +0000

    Added MeetingRoom class, added dependencies for topological data types

---
+++ Seesaw/Models/MeetingRooms.cs
@@ -0,0 +1,15 @@
+﻿using NetTopologySuite.Geometries;
+using System.Collections.Generic;
+
+namespace Seesaw.Models
+{
+    public class MeetingRoom
+    {
+        public int Id { get; set; }
+        //public Unit ContainingUnit { get; set; }
+        public string DisplayName { get; set; }
+        public Polygon BoundingBox { get; set; }
+        public int Capacity { get; set; }
+        //public List<MeetingRoomAttributes> Attributes { get; set; }
+    }
+}
--- Seesaw/SeesawDbContext.cs
+++ Seesaw/SeesawDbContext.cs
@@ -6,10 +6,11 @@ namespace Seesaw
     public class SeesawDbContext : DbContext
     {
         public DbSet<Office> Offices { get; set; }
-        
+        public DbSet<MeetingRoom> MeetingRooms { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         { 
-            optionsBuilder.UseNpgsql(@"User ID=seesaw;Password=seesaw;Host=localhost;Port=5432;Database=seesaw;");
+            optionsBuilder.UseNpgsql(@"User ID=seesaw;Password=seesaw;Host=localhost;Port=5432;Database=Seesaw-Dev;", x=> x.UseNetTopologySuite());
         }
     }
 }
